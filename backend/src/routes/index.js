e8bbd138a4dba61e8ae42c03f5efd769b5c704d2
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-02-19 00:37:56 -0300

    fix(backend) - Corrigindo endpoint da autenticação.

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const usersRouter = require('./users.routes');
+const sessionsRouter = require('./sessions.routes');
 const routes = express.Router();
 
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 module.exports = routes;
